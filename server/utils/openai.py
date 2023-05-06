13ca93175cc3aeab2f1a5bca1ba7ec7d0404b482
theanhtran372000 <trantheanh.ktmt@gmail.com>
2023-05-07 02:42:17 +0700

    feat: add tts, stt, answer to server

---
+++ server/utils/openai.py
@@ -0,0 +1,12 @@
+import openai
+
+def get_chatgpt_answer(prompt, model_name='gpt-3.5-turbo', role='user'):
+    completion = openai.ChatCompletion.create(
+        model=model_name,
+        messages=[{
+            'role': role, # user/assistant (mean ChatGPT)/system
+            'content': prompt
+        }]
+    )
+    
+    return completion['choices'][0]['message']['content']
